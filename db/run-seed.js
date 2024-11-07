1afa113cc01c36dd598e9da01d90443d7787fd10
AkivaKn <akivakaufman@gmail.com>
2024-11-07 10:44:47 +0000

    feat: seed database with example data

--- db/run-seed.js
+++ db/run-seed.js
@@ -1,8 +1,9 @@
 const db = require('./index');
-const seed = require('./seed')
+const seed = require('./seed');
+const devData = require('./data.json')
 
 const runSeed = () => {
-  return seed().then(() => db.end());
+  return seed(devData).then(() => db.end());
 };
 
 runSeed();
--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,16 @@
+const format = require("pg-format");
 const db = require("./index");
 
-async function seed() {
+async function seed({
+  users,
+  user_profiles,
+  posts,
+  post_likes,
+  post_comments,
+  post_comment_likes,
+  post_comment_replies,
+  post_comment_reply_likes,
+}) {
   await db.query(`DROP TABLE IF EXISTS post_comment_reply_likes;`);
   await db.query(`DROP TABLE IF EXISTS post_comment_replies;`);
   await db.query(`DROP TABLE IF EXISTS post_comment_likes;`);
@@ -63,50 +73,117 @@ async function seed() {
         "username" VARCHAR UNIQUE,
         "profile_picture_url" VARCHAR,
         "bio" VARCHAR,
-        "joined_at" TIMESTAMP
+        "joined_at" TIMESTAMP DEFAULT NOW()
       );`);
 
   await db.query(`CREATE TABLE "posts" (
         "post_id" SERIAL PRIMARY KEY,
         "caption" VARCHAR,
-        "user_id" INT REFERENCES users(user_id),
-        "private" boolean,
+        "user_profile_id" INT REFERENCES user_profiles(user_profile_id) NOT NULL,
+        "private" boolean NOT NULL,
         "created_at" TIMESTAMP DEFAULT NOW()
       );`);
 
   await db.query(` CREATE TABLE "post_likes" (
             "post_like_id" SERIAL PRIMARY KEY,
-            "post_id" INT REFERENCES posts(post_id),
-            "user_id" INT REFERENCES users(user_id),
+            "post_id" INT REFERENCES posts(post_id) NOT NULL,
+            "user_profile_id" INT REFERENCES user_profiles(user_profile_id) NOT NULL,
             "created_at" TIMESTAMP DEFAULT NOW()
           );`);
   await db.query(` CREATE TABLE "post_comments" (
         "post_comment_id" SERIAL PRIMARY KEY,
-        "post_id" INT REFERENCES posts(post_id),
-        "comment_body" VARCHAR,
-        "user_id" INT REFERENCES users(user_id),
+        "post_id" INT REFERENCES posts(post_id) NOT NULL,
+        "comment_body" VARCHAR(2000) NOT NULL,
+        "user_profile_id" INT REFERENCES user_profiles(user_profile_id) NOT NULL,
         "created_at" TIMESTAMP DEFAULT NOW()
       );`);
 
   await db.query(`CREATE TABLE "post_comment_likes" (
             "post_comment_like_id" SERIAL PRIMARY KEY,
-            "post_comment_id" INT REFERENCES post_comments(post_comment_id),
-            "user_id" INT REFERENCES users(user_id),
+            "post_comment_id" INT REFERENCES post_comments(post_comment_id) NOT NULL,
+            "user_profile_id" INT REFERENCES user_profiles(user_profile_id) NOT NULL,
             "created_at" TIMESTAMP DEFAULT NOW()
           );`);
   await db.query(` CREATE TABLE "post_comment_replies" (
         "post_comment_reply_id" SERIAL PRIMARY KEY,
-        "post_comment_id" INT REFERENCES post_comments(post_comment_id),
-        "reply_body" VARCHAR,
-        "user_id" INT REFERENCES users(user_id),
+        "post_comment_id" INT REFERENCES post_comments(post_comment_id) NOT NULL,
+        "reply_body" VARCHAR NOT NULL,
+        "user_profile_id" INT REFERENCES user_profiles(user_profile_id) NOT NULL,
         "created_at" TIMESTAMP DEFAULT NOW()
       );`);
 
   await db.query(`CREATE TABLE "post_comment_reply_likes" (
         "post_comment_reply_like_id" SERIAL PRIMARY KEY,
-        "comment_reply_id" INT REFERENCES post_comment_replies(post_comment_reply_id),
-        "user_id" INT REFERENCES users(user_id),
+        "comment_reply_id" INT REFERENCES post_comment_replies(post_comment_reply_id) NOT NULL,
+        "user_profile_id" INT REFERENCES user_profiles(user_profile_id) NOT NULL,
         "created_at" TIMESTAMP DEFAULT NOW()
       );`);
+  const insertUsersQueryStr = format(
+    "INSERT INTO users (name, email, email_verified, phone_number, image) VALUES %L;",
+    users.map(({ name, email, email_verified, phone_number, image }) => [
+      name,
+      email,
+      email_verified,
+      phone_number,
+      image,
+    ])
+  );
+  await db.query(insertUsersQueryStr);
+
+  const insertUserProfilesQueryStr = format(
+    "INSERT INTO user_profiles (user_id, username, profile_picture_url, bio, joined_at) VALUES %L;",
+    user_profiles.map(({ user_id, username, profile_picture_url, bio, joined_at }) => [
+      user_id, username, profile_picture_url, bio, joined_at
+    ])
+  );
+  await db.query(insertUserProfilesQueryStr);
+
+  const insertPostsQueryStr = format(
+    "INSERT INTO posts (caption, user_profile_id, private, created_at) VALUES %L;",
+    posts.map(({caption, user_profile_id, private, created_at }) => [
+      caption, user_profile_id, private, created_at
+    ])
+  );
+  await db.query(insertPostsQueryStr);
+
+  const insertPostLikesQueryStr = format(
+    "INSERT INTO post_likes (post_id, user_profile_id, created_at) VALUES %L;",
+    post_likes.map(({ post_id, user_profile_id, created_at }) => [
+      post_id, user_profile_id, created_at
+    ])
+  );
+  await db.query(insertPostLikesQueryStr);
+
+  const insertPostCommentsQueryStr = format(
+    "INSERT INTO post_comments (post_id, comment_body, user_profile_id, created_at) VALUES %L;",
+    post_comments.map(({post_id, comment_body, user_profile_id, created_at }) => [
+      post_id, comment_body, user_profile_id, created_at
+    ])
+  );
+  await db.query(insertPostCommentsQueryStr);
+  
+  const insertPostCommentLikesQueryStr = format(
+    "INSERT INTO post_comment_likes (post_comment_id, user_profile_id, created_at) VALUES %L;",
+    post_comment_likes.map(({ post_comment_id, user_profile_id, created_at }) => [
+      post_comment_id, user_profile_id, created_at
+    ])
+  );
+  await db.query(insertPostCommentLikesQueryStr);
+  
+  const insertPostCommentRepliesQueryStr = format(
+    "INSERT INTO post_comment_replies (post_comment_id, reply_body, user_profile_id, created_at) VALUES %L;",
+    post_comment_replies.map(({ post_comment_id, reply_body, user_profile_id, created_at }) => [
+      post_comment_id, reply_body, user_profile_id, created_at
+    ])
+  );
+  await db.query(insertPostCommentRepliesQueryStr);
+  
+  const insertPostCommentReplyLikesQueryStr = format(
+    "INSERT INTO post_comment_reply_likes (comment_reply_id, user_profile_id, created_at) VALUES %L;",
+    post_comment_reply_likes.map(({ comment_reply_id, user_profile_id, created_at }) => [
+      comment_reply_id, user_profile_id, created_at
+    ])
+  );
+  await db.query(insertPostCommentReplyLikesQueryStr);
 }
 module.exports = seed;
