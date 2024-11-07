dbae28d8e5e93f2af317a09f11c0dbaffa219544
AkivaKn <akivakaufman@gmail.com>
2024-11-07 12:54:51 +0000

    feat: add error handling for non existent endpoint

--- api/app.js
+++ api/app.js
@@ -1,10 +1,15 @@
-const express = require("express")
+const express = require("express");
 const apiRouter = require("./routes/api_router");
+const { customErrors } = require("./error_handling");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api",apiRouter)
+app.use("/api", apiRouter);
+
+app.all("/*", (req, res, next) => next({ status: 404, msg: "Path not found" }));
+
+app.use(customErrors);
 
 module.exports = app;
--- api/routes/api_router.js
+++ api/routes/api_router.js
@@ -1,6 +1,7 @@
 const apiRouter = require("express").Router();
 const { getApis } = require("../controllers/api_controller");
+const { methodNotAllowed } = require('../error_handling');
 
-apiRouter.route("/").get(getApis);
+apiRouter.route("/").get(getApis).all(methodNotAllowed);
 
 module.exports = apiRouter;
